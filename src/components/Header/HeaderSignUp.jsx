d7de325f379b7fb0e648b0c44d9fe6f89aaa4b68
Mahmut Aktaş <mahmutaktas.m@gmail.com>
2024-06-14 19:40:57 +0300

    HeaderSignUp sayfası bitti

--- src/components/Header/HeaderSignUp.jsx
+++ src/components/Header/HeaderSignUp.jsx
@@ -1,5 +1,7 @@
+import styled from "styled-components";
+import { Link, useHistory } from "react-router-dom";
+import { useState } from "react";
 
-import styled from "styled-components"
 import backgroundImage from "../../assets/the_netflix_login_background__canada__2024___by_logofeveryt_dh0w8qv-414w-2x.jpg";
 
 const BackgroundImage = styled.div`
@@ -17,11 +19,11 @@ const BackgroundImage = styled.div`
 const Overlay = styled.div`
   width: 100%;
   height: 100vh;
-  background: rgba(0, 0, 0, 0.5); 
+  background: rgba(0, 0, 0, 0.5);
   position: fixed;
   top: 0;
   left: 0;
-  z-index: -1; 
+  z-index: -1;
 `;
 
 const HeaderArea = styled.header`
@@ -33,21 +35,20 @@ const HeaderArea = styled.header`
   padding: 0 1rem;
   gap: 2rem;
   z-index: 2;
-  top:0.7rem;
+  top: 0.7rem;
   left: 0;
-  position: fixed
+  position: fixed;
 `;
-
 
 const Logo = styled.p`
   font-size: 2.5rem;
-  color: #E50914;;
+  color: #e50914;
   font-weight: bold;
   margin-left: 10rem;
   cursor: pointer;
 `;
 
-const SignIn = styled.button`
+const SignIn = styled(Link)`
   width: 6%;
   background: red;
   color: white;
@@ -56,41 +57,40 @@ const SignIn = styled.button`
   text-align: center;
   border-radius: 0.5rem;
   cursor: pointer;
-  margin-right: 12rem; 
+  margin-right: 12rem;
 `;
 
 const H1 = styled.h1`
-text-align: center;
-justify-content: center;
+  text-align: center;
 `;
 
 const H2 = styled.h2`
-text-align: center;
-justify-content: center;
-`
+  text-align: center;
+`;
+
 const H3 = styled.h3`
-text-align: center;
-justify-content: center;
-`
+  text-align: center;
+`;
+
+const FormContainer = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1rem;
+`;
+
 const Input = styled.input`
-  display: block;
-  width: 50%;
+  flex: 1;
   padding: 0.5rem;
   font-size: 1rem;
-  margin-top: 0.5rem;
+  margin-right: 0.5rem;
   background-color: rgba(255, 255, 255, 0.2);
-  justify-content: center;
-  ::placeholder { 
-    color: white !important; 
-    opacity: 0.7; 
+  ::placeholder {
+    color: white !important;
+    opacity: 0.7;
   }
-    
-  &.inline-input {
-    display: inline-block;
-    margin-right: 1rem;
-    width: 20rem;
-    
-  }
+  max-width: 300px;
 `;
 
 const GetStarted = styled.button`
@@ -100,24 +100,92 @@ const GetStarted = styled.button`
   text-align: center;
   border-radius: 0.2rem;
   cursor: pointer;
-  
+  border: none;
 `;
 
+const ErrorMessage = styled.p`
+  padding: 0.5rem;
+  color: red;
+  background: transparent;
+  font-weight: 700;
+  border-radius: 0.3rem;
+  margin-top: 0.5rem;
+  text-align: center;
+  width: 20%;
+  margin-left: 14.5rem;
+`;
+
+const errorMessages = {
+  email: "E-posta adresi gerekli.",
+};
+
+const initialFormData = {
+  email: "",
+};
 
 export default function HeaderSignUp() {
+  const [formData, setFormData] = useState(initialFormData);
+  const [errors, setErrors] = useState({
+    email: false,
+  });
+  const history = useHistory();
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+
+    if (name === "email") {
+      if (validateEmail(value)) {
+        setErrors({ ...errors, email: false });
+      } else {
+        setErrors({ ...errors, email: true });
+      }
+    }
+  }
+
+  function handleGetStarted(event) {
+    event.preventDefault();
+
+    if (!formData.email) {
+      setErrors({ ...errors, email: true });
+      return;
+    }
+
+    history.push("/Login");
+  }
+
   return (
     <>
-    <BackgroundImage />
-    <Overlay/>
-    <HeaderArea>
-    <Logo>WitFlix</Logo>
-    <SignIn>Sing In</SignIn>
-   </HeaderArea>
-   <H1>Unlimited movies, TV shows, and more</H1>
-   <H2>Watch anywhere. Cancel anytime.</H2>
-   <H3>Ready to watch? Enter your email to create or restart your membership.</H3>
-   <Input className="inline-input"  placeholder="Enter your email"/>
-    <GetStarted>Get Started</GetStarted>
-  </>
-);
+      <BackgroundImage />
+      <Overlay />
+      <HeaderArea>
+        <Logo>WitFlix</Logo>
+        <SignIn to="/Login">Giriş Yap</SignIn>
+      </HeaderArea>
+      <H1>Sınırsız film, dizi ve daha fazlası</H1>
+      <H2>Her yerde izleyin. İstediğiniz zaman iptal edin.</H2>
+      <H3>
+        İzlemeye hazır mısınız? Üyeliğinizi oluşturmak veya yeniden başlatmak
+        için e-posta adresinizi girin.
+      </H3>
+      <FormContainer onSubmit={handleGetStarted}>
+        <Input
+          placeholder="E-posta adresinizi girin"
+          value={formData.email}
+          onChange={handleChange}
+          name="email"
+        />
+        <GetStarted type="submit">Başlayın</GetStarted>
+      </FormContainer>
+      {errors.email && <ErrorMessage>{errorMessages.email}</ErrorMessage>}
+    </>
+  );
 }
